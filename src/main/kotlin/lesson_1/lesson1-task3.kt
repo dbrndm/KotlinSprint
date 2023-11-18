cb132a50e85ec9008231a363de3f4cdf1e3b42e2
dbrndm <dobronozhenko.dmitriy@gmail.com>
2023-11-18 18:12:07 +0300

    KS-1-3-first-space-flight

---
+++ src/main/kotlin/lesson_1/lesson1-task3.kt
@@ -0,0 +1,14 @@
+package lesson_1
+
+fun main() {
+    val yearFirstSpaceFlight  = 1961
+    var hourFlight = "09"
+    var minuteFlight = "07"
+    println(yearFirstSpaceFlight)
+    println(hourFlight)
+    println(minuteFlight)
+
+    hourFlight = "10"
+    minuteFlight = "55"
+    println("$hourFlight:$minuteFlight")
+}
