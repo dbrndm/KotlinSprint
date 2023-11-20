6729f84d32e6e6669210e1e37cbf01998f0e8f49
dbrndm <dobronozhenko.dmitriy@gmail.com>
2023-11-20 20:30:34 +0300

    KS-2-1 refactor

--- src/main/kotlin/lesson_2/lesson2-task1.kt
+++ src/main/kotlin/lesson_2/lesson2-task1.kt
@@ -1,7 +1,14 @@
 package lesson_2
 
 fun main() {
-    val arithmeticMean = (3+4+3+5)/4.00
+    val a = 3.0
+    val b = 4.0
+    val c = 3.0
+    val d = 5.0
+
+    val numberOfStudents = 4
+
+    val arithmeticMean = (a + b + c + d) / numberOfStudents
 
     println(arithmeticMean)
 }
