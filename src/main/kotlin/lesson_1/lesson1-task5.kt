f62023224e922545c9ae3e722972233124d82a1e
dbrndm <dobronozhenko.dmitriy@gmail.com>
2023-11-20 20:20:57 +0300

    KS-2-3 add constant and String-format

--- src/main/kotlin/lesson_1/lesson1-task5.kt
+++ src/main/kotlin/lesson_1/lesson1-task5.kt
@@ -1,5 +1,7 @@
 package lesson_1
 
+const val sizeTime = 60
+
 fun main() {
 /*
 - Переведи в коде количество секунд в целые минуты и целые часы, сохраняя остаток секунд и минут в новые переменные;
@@ -9,13 +11,14 @@ fun main() {
 
     val secondsFlight = 6480
 
-    val hour = secondsFlight/3600
-    val hourRemainder = secondsFlight%3600 // остаток от деления
+    val hour = secondsFlight / (sizeTime * sizeTime)
+    val hourRemainder = secondsFlight % (sizeTime * sizeTime) // остаток от деления
 
-    val minutes = hourRemainder/60
-    val minutesRemainder = hourRemainder%60 // остаток от деления
+    val minutes = hourRemainder / sizeTime
+    val minutesRemainder = hourRemainder % sizeTime // остаток от деления
 
-    val seconds = minutesRemainder/60
+    val seconds = minutesRemainder / sizeTime
 
-    println("Время, проведенное в космосе: 0$hour:$minutes:0$seconds")
+    val time = String.format("%02d:%02d:%02d", hour, minutes, seconds)
+    println("Время, проведенное в космосе: $time")
 }
