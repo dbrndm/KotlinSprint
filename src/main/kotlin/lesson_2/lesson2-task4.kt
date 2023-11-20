36626e7603e77baff87490b8fa67c3d36733246f
dbrndm <dobronozhenko.dmitriy@gmail.com>
2023-11-20 21:01:34 +0300

    KS-2-4 mining

---
+++ src/main/kotlin/lesson_2/lesson2-task4.kt
@@ -0,0 +1,14 @@
+package lesson_2
+
+fun main() {
+    val crystalline = 7
+    val iron = 11
+
+    val buff = 0.2
+
+    val crystallineBuff = crystalline * buff
+    val ironBuff = iron * buff
+
+    println(crystallineBuff.toInt())
+    println(ironBuff.toInt())
+}
