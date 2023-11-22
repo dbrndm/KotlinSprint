0ebc7e454f391bb5889752402b607a322ef0e0f9
dbrndm <dobronozhenko.dmitriy@gmail.com>
2023-11-22 21:28:05 +0300

    KS-3-4 chess

---
+++ src/main/kotlin/lesson_3/lesson3-task4.kt
@@ -0,0 +1,15 @@
+package lesson_3
+
+fun main() {
+    var startingPoint = "E2"
+    var endPoint = "E4"
+    var strokeNumber = 1
+
+    println("$startingPoint-$endPoint;$strokeNumber")
+
+    startingPoint = "D2"
+    endPoint = "D3"
+    strokeNumber = 2
+    println("$startingPoint-$endPoint;$strokeNumber")
+
+}
